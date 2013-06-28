d24048bc8946b1a258a45aa4157f8b451d654707
schrink <vlelicanin@strategicpoint.rs>
2013-06-28 14:04:26 +0200

    Template stranica za vasu bazu

---
+++ index.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html>
+  <head>
+		<title>Vezbanje PHP MySql</title>
+	
+	<style>
+	ul {
+		width: 400px;
+		margin: 0 auto;
+		list-style-type: none;
+	}
+	li {
+		float: left;
+		padding: 10px;
+		background-color: #ccc;
+	}
+	.clear {
+		clear:both;
+		margin-bottom: 20px;
+	}
+	table {
+		width: 100%;
+
+	}
+
+	td {
+		padding: 3px 20px;
+		border: 1px solid #ccc;
+		margin: 0;
+		text-align: center;
+
+	}
+
+	td img {
+		height: 100px;
+	}
+
+	</style>
+	</head>
+
+	<body>
+		<ul>
+			<li><a href="#">Svi filmovi</a></li><!-- OVO LISTA SVE FILMOVE U DONJOJ TABELI --> 
+			<li><a href="#">Strani</a></li><!-- OVO LISTA SAMO STRANE U  DONJOJ TABELI --> 
+			<li><a href="#">Domaci</a></li>
+			<li><a href="#">Unos novog filma</a></li><!-- OVO IZBACUJE FORMULAR ZA UNOS NOVOG --> 
+		</ul>
+	<div class="clear"></div>
+	<table>
+		<thead>
+			<tr>
+				<th>Rbr</th>
+				<th>Naziv Filma</th>
+				<th>Slika</th>
+				<th>Reditelj</th>
+				<th>Glumci</th>
+				<th>Godina</th>
+				<th>Kategorija</th>
+				<th>Akcije</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>1</td>
+				<td>Shaun of the Dead</td>
+				<td><img src="images/shawn.jpg" alt=""></td>
+				<td>Edgar Wright</td>
+				<td>Simon Pegg, Kate Ashfield, Nick Frost</td>
+				<td>2004</td>
+				<td>Strani</td>
+				<td><a href="">Edit</a> | <a href="">Delete</a></td>
+			</tr>
+		</tbody>
+	</table>
+	</body>
+</html>
